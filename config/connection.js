aaa87b4554518ecb8ae61e6366a6def0e5b095d6
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-06-21 16:30:35 -0500

    added config

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+//call for sequelize
+var Sequelize = require("sequelize");
+
+//create mySQL connection using Sequelize
+var sequelize = new Sequelize('takoloko_db', 'm3mljqs6i6omozbl', 'wuc1jyhxtaoj8l9w', {
+    host: 's54ham9zz83czkff.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+});
+
+
+
+//exporting connection to be available for other files
+module.exports = sequelize;
