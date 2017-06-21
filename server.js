08185bd4c755c17216eea97550614721348a7c3e
GeekInterrupted <GeekInterrupted@users.noreply.github.com>
2017-06-20 21:51:38 -0500

    changed files

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ var app = express();
 var db = require("./models");
 
 //serve static content for the app from the public directory in the application directory
-app.use(express.static(__dirname + "/"));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(__dirname + "/public"));
 // app.use(express.static(process.cwd() + "/"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //override with POST having ?_method=DELETE
